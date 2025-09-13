5580e41d28a074548ee350ae6661cbb514b730ec
Gregory Filatov <grigorijfilatov@gmail.com>
2025-09-13 16:24:16 +0300

    Initial

---
+++ embedded/tests/memory/inc/test_mem.h
@@ -0,0 +1,37 @@
+/**
+ * ---------------------------------------------------------------------------
+ * Copyright (c) 2025 Gregory Filatov
+ * All rights reserved.
+ *
+ * \file   test_mem.h
+ * \author Gregory Filatov
+ * \date   30 May 2025
+ * \version 1.0
+ *
+ * \brief  Contains memory tests
+ *
+ * Module: Tests
+ *
+ * Purpose: Example - file
+ *
+ * Functionality:
+ *     Environment
+ * ---------------------------------------------------------------------------
+ */
+
+#ifndef TEST_MEM_H_
+#define TEST_MEM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TEST_MEM_H_ */
---
+++ embedded/tests/memory/src/test_mem.c
@@ -0,0 +1,21 @@
+/**
+ * ---------------------------------------------------------------------------
+ * Copyright (c) 2025 Gregory Filatov
+ * All rights reserved.
+ *
+ * \file   test_mem.c
+ * \author Gregory Filatov
+ * \date   30 May 2025
+ * \version 1.0
+ *
+ * \brief  Contains memory tests
+ *
+ * Module: Tests
+ *
+ * Purpose: Example - file
+ *
+ * Functionality:
+ *     Environment
+ * ---------------------------------------------------------------------------
+ */
+#include "test_mem.h"
